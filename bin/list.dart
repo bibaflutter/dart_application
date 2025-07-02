028e2e15ed936c026ee2d3914e9fdc5786a52dfa
bibaflutter <bibok2601@gmail.com>
2025-07-02 16:21:24 +0500

    if else lesson code

--- bin/list.dart
+++ bin/list.dart
@@ -1,4 +1,47 @@
+// 1) doubles parse num - double, int 
+// void main() {
+//   String myString = '10.2';
+//   print(double.tryParse(myString));
+// }
+
+// 2) Чо то сложное хз
+// Математическая операция но перед этим строку распарсили
+
+// void main() {
+//   String myString = '10.2';
+//   String myStringTwo = '10.2';
+//   double myDouble = double.tryParse(myString) ?? 0;
+//   double myDoubleTwo = double.tryParse(myStringTwo) ?? 0;
+
+//   print(myDouble + myDoubleTwo);
+// }
+
+
+// 3) Обьявление данных в Dart
+
+/*
+Dart Dynamic and Const
+
+Final - неизменяеммый, но инициалзируемый
+Var - изменяеммый, но запоминающим типом
+Dynamic - изменяеммый , с возможностью изменить даже тип данных
+Const - неизменняемый, но инициализируемый 
+
+*/
+
+// void main() {
+//   final name = 'Abai'; // переменная name не сможет менятся. Например переназначить переменную внизу
+//   print(name.runtimeType);
+
+
+//   var name1 = 'Tekimbaev';
+//   name1 = 'Tekimbaev 0000'; // вот например var позволяет нам переназначить переменную, а final не пощволила бы
+//   print(name1);
+// }
+
+
+// Const
+
 void main() {
-  String myString = '10.2';
-  print(double.tryParse(myString));
+  
 }
--- bin/list_part2.dart
+++
@@ -1,4 +0,0 @@
-void main() {
-  String myString = '10.2';
-  print(double.tryParse(myString));
-}
